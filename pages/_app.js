2684b34312476ff4c7b2942db300cfe2d66aa300
Michael Koch <126342407+mikekochh@users.noreply.github.com>
2023-08-19 10:59:06 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/673be014b0.js" crossorigin="anonymous"></Script>
+  </ThemeProvider>
+);
 
 export default MyApp;
