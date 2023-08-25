f9023b2477c8805ed4d424ded68fbface86192f9
Michael Koch <126342407+mikekochh@users.noreply.github.com>
2023-08-25 12:26:50 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div>
+            <Image src={image.logo2} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Michael Koch</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
