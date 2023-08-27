33e1ef60348bae0299388e4ec5cb969f7ca937b2
mikekochh <michaelkoch4444@gmail.com>
2023-08-26 20:35:03 -0400

    Final touches on nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -92,7 +92,18 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex ml-2">
-        {isOpen ? (<Image src={images.cross} />)
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="menu"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' && 'filter invert'}
+
+          />
+        )
           : (
             <Image
               src={images.menu}
@@ -102,8 +113,20 @@ const Navbar = () => {
               alt="menu"
               onClick={() => setIsOpen(true)}
               className={theme === 'light' && 'filter invert'}
+              // filter invert is inverting the colors on the symbol
             />
           )}
+
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems active={active} setActive={setActive} isMobile />
+          </div>
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+        )}
       </div>
     </nav>
   );
